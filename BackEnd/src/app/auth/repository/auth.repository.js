3268b8257be7aa91d09763d3548b8c3efcd32462
Karim Omar <omarkareem409@gmail.com>
2026-09-15 19:00:09 +0300

    editing createOtp function in ./backend/src/app/auth/repository/auth.repository.js

--- BackEnd/src/app/auth/repository/auth.repository.js
+++ BackEnd/src/app/auth/repository/auth.repository.js
@@ -1,10 +1,13 @@
 import OTP from '../model/otp.model.js';
 import crypto from "crypto";
-export const createOtp = async (email, minutes) => {
+export const createOtp = async (email, minutes = 5) => {
     const otp = crypto.randomInt(100000, 1000000).toString();
-    return await OTP.create({
-        code : otp,
-        email : email,
+
+    await OTP.deleteOne({ email });
+
+    return OTP.create({
+        code: otp,
+        email,
         expiresAt: new Date(Date.now() + minutes * 60 * 1000)
-    })
-}
+    });
+};
